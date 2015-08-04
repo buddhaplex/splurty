2f90e80d0b26ba72634bb703713a885b52cdfbbd
Eric Andrade <ericandrade@gmail.com>
2015-08-04 02:50:00 +0000

    First TDD test passed

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-     assert true
-  end
+
+	test "unique_tag" do
+		quote = Quote.create(:author => 'Charles Bukowski', :saying => 'Some people never go crazy. What truly horrible lives they must lead.')
+		expected = 'CB#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
+
 end
